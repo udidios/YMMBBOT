2cabac125b0faeb1b97a2d406b2e556c8d15cd79
udidios <99913860+udidios@users.noreply.github.com>
2023-11-29 20:20:54 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-BOT_TOKEN = ''  # Токен от @BotFather
-YANDEX_MUSIC_TOKEN = ''  # Токен от Yandex-music-api
-YOUR_CHANNEL = '@EXAMPLECHANNEL'  # Id канала/Username канала, куда надо отправлять текст. Пример: '@examplechannel'
+BOT_TOKEN = '6783571973:AAElO-NvJRZFLSolLilA_tAZJxsDa1V8URI'  # Токен от @BotFather
+YANDEX_MUSIC_TOKEN = 'y0_AgAAAAAFs6RGAAG8XgAAAADzUzhlxqYEYSnaSVigmr1L5G8be1vJtYM'  # Токен от Yandex-music-api
+YOUR_CHANNEL = '@YMMBBOT2'  # Id канала/Username канала, куда надо отправлять текст. Пример: '@examplechannel'
 LASTFM_API_KEY = ''  # Api-key от last.fm
 LASTFM_API_SECRET = ''  # Secret api-key от last.fm
 LASTFM_USERNAME = ''  # Ваш username на last.fm
